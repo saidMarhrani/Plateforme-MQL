5ff387777fc489a15f85470e0c7176a761b691ce
Oukirche <abdessamade.oukirche@gmail.com>
2020-05-17 09:10:51 +0000

    add partenaire page

---
+++ js/footer.js
@@ -0,0 +1,70 @@
+document.write("\n" +
+    "\n" +
+    "    <footer>\n" +
+    "        <div class=\"row col-12 col-s-12\">\n" +
+    "            <!-- <div class=\"in-row\">\n" +
+    "                <h3 class=\"content-header\">Div 1</h3>\n" +
+    "            </div> -->\n" +
+    "\n" +
+    "            <div class=\"in-row col-4 col-s-8\">\n" +
+    "                <h3 class=\"content-header\">Information</h3>\n" +
+    "                <div class=\"content-body pStyle\">\n" +
+    "                    <p class=\"space-line\"><a class=\"footer-link\" href=\"#\">A propos de MQL</a></p>\n" +
+    "                    <!-- <hr /> -->\n" +
+    "                    <p class=\"space-line\"><a class=\"footer-link\" href=\"#\">Evenements</a></p>\n" +
+    "                    <!-- <hr /> -->\n" +
+    "                    <p class=\"space-line\"><a class=\"footer-link\" href=\"#\">Différents Partenaires</a></p>\n" +
+    "                    <!-- <hr /> -->\n" +
+    "                    <p class=\"space-line\"><a class=\"footer-link\" href=\"#\">Activités</a></p>\n" +
+    "                    <!-- <hr /> -->\n" +
+    "                    <p class=\"space-line\"><a class=\"footer-link\" href=\"#\">Plus de recherche</a></p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "\n" +
+    "            <div class=\"in-row col-4 col-s-8\">\n" +
+    "                <h3 class=\"content-header\">A propos de MQL</h3>\n" +
+    "                <div class=\"content-body pStyle\">\n" +
+    "                    <p class=\"space-line\"><a class=\"footer-link\" href=\"#\">Objectif de la formation</a></p>\n" +
+    "                    <!-- <hr /> -->\n" +
+    "                    <p class=\"space-line\"><a class=\"footer-link\" href=\"#\">Débouchés de la Formation</a></p>\n" +
+    "                    <!-- <hr /> -->\n" +
+    "                    <p class=\"space-line\"><a class=\"footer-link\" href=\"#\">Conditions d’accès</a></p>\n" +
+    "                    <br />\n" +
+    "                    <div>\n" +
+    "                        <a href=\"#\"><img class=\"img-size lang\" src=\"../icons/english.png\" title=\"Anglais\" /></a>\n" +
+    "                        <a href=\"#\"><img class=\"img-size lang\" src=\"../icons/frensh.png\" title=\"Français\" /></a>\n" +
+    "                        <a href=\"#\"><img class=\"img-size lang\" src=\"../icons/saudi-arabia.png\" title=\"Arabe\" /></a>\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "\n" +
+    "            <div class=\"in-row col-4 col-s-8\">\n" +
+    "                <h3 class=\"content-header\">Contactez nous</h3>\n" +
+    "                <div class=\"content-body contactus\">\n" +
+    "                    <p class=\"space-line\"><img class=\"img-size\" src=\"../icons/maps-and-flags.png\" align=\"middle\" />\n" +
+    "                        <span>Département d’Informatique, Faculté des sciences Dhar El Mahraz BP.1796,\n" +
+    "                            Fès-Atlas, Maroc.</span></p>\n" +
+    "                    <p class=\"space-line\"><img class=\"img-size\" src=\"../icons/celular.png\" align=\"middle\" /> <span>+212\n" +
+    "                            6 00 00 00 00</span></p>\n" +
+    "                    <p class=\"space-line\"><img class=\"img-size\" src=\"../icons/emails.png\" align=\"middle\" />\n" +
+    "                        <span>fsdm.mql@gmail.com</span></p>\n" +
+    "                    <p class=\"space-line\"><img class=\"img-size\" src=\"../icons/website.png\" align=\"middle\" />\n" +
+    "                        <span>www.fsdm.usmba.ac.ma</span></p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "\n" +
+    "        <div id=\"social-copy col-12 col-s-12\">\n" +
+    "            <hr />\n" +
+    "            <div id=\"socialmedia\" class=\"col-4 col-s-8\">\n" +
+    "                <a href=\"#\"><img class=\"img-size lang\" id=\"facebook\" src=\"../icons/facebook.png\" title=\"Facebook\" /></a>\n" +
+    "                <a href=\"#\"><img class=\"img-size lang\" src=\"../icons/social-media.png\" title=\"Google+\" /></a>\n" +
+    "                <a href=\"#\"><img class=\"img-size lang\" src=\"../icons/twitter.png\" title=\"Twitter\" /></a>\n" +
+    "                <a href=\"#\"><img class=\"img-size lang\" src=\"../icons/instagram.png\" title=\"Instagram\" /></a>\n" +
+    "                <br />\n" +
+    "                <p>MQL &copy 2019 - 2021</p>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "\n" +
+    "    </footer>\n" +
+    "    ");
---
+++ js/header.js
@@ -0,0 +1,51 @@
+document.write(" <nav>\n" +
+    "       <div class=\"bar-menu\">\n" +
+    "           <ul>\n" +
+    "               <li><img src=\"../images/MQLTransaprent.png\" alt=\"logo\" style=\"width: 100px; height: 50px;\"></li>\n" +
+    "               <li><a href=\"#\">Prérequis</a>\n" +
+    "                <div class=\"sub-menu-1\">\n" +
+    "                    <ul>\n" +
+    "                        <li><a href=\"#\">Profils sélectionnés</a></li>\n" +
+    "                        <li><a href=\"#\">Qualités nécessaires</a></li>\n" +
+    "                    </ul>\n" +
+    "                </div>\n" +
+    "               </li>\n" +
+    "               <li><a href=\"#\">Activités</a>\n" +
+    "                   <div class=\"sub-menu-1\">\n" +
+    "                       <ul>\n" +
+    "                           <li><a href=\"#\">Cours</a></li>\n" +
+    "                           <li><a href=\"#\">Travaux individuels</a></li>\n" +
+    "                           <li><a href=\"#\">Travaux d’équipes</a></li>\n" +
+    "                           <li><a href=\"#\">Présentations</a></li>\n" +
+    "                           <li><a href=\"#\">Soft Skills</a></li>\n" +
+    "                       </ul>\n" +
+    "                   </div>\n" +
+    "               </li>\n" +
+    "               <li><a href=\"#\">Evénements</a>\n" +
+    "                   <div class=\"sub-menu-1\">\n" +
+    "                       <ul>\n" +
+    "                           <li><a href=\"#\">Compagnes de stages</a></li>\n" +
+    "                           <li><a href=\"#\">Tests psychotechniques</a></li>\n" +
+    "                           <li><a href=\"#\">Tests techniques</a></li>\n" +
+    "                           <li><a href=\"#\">Entretiens</a></li>\n" +
+    "                           <li><a href=\"#\">Remise des diplômes </a></li>\n" +
+    "                       </ul>\n" +
+    "                   </div>\n" +
+    "               </li>\n" +
+    "               <li><a href=\"#\">Partenaires</a>\n" +
+    "                   <div class=\"sub-menu-1\">\n" +
+    "                       <ul>\n" +
+    "                           <li><a href=\"#\">Capgemini</a></li>\n" +
+    "                           <li><a href=\"#\">CGI</a></li>\n" +
+    "                           <li><a href=\"#\">Atos</a></li>\n" +
+    "                           <li><a href=\"#\">Umanis</a></li>\n" +
+    "                           <li><a href=\"#\">SQLI</a></li>\n" +
+    "                       </ul>\n" +
+    "                   </div>\n" +
+    "               </li>\n" +
+    "               <li><a href=\"#\">Lauréats</a></li>\n" +
+    "           </ul>\n" +
+    "       </div>\n" +
+    "    </nav>\n" +
+    "\n" +
+    "\n");
